17e88176df0876279e97a067dfbefa5f9d64b434
Alexey Ariskin <65245105+screwdriver2211@users.noreply.github.com>
2022-06-17 10:26:05 +0300

    Lab load

---
+++ Lab 9 Ex1/MeasuringDevice/ILoggingMeasuringDevice.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MeasuringDevice
+{
+    public interface ILoggingMeasuringDevice :IMeasuringDevice
+    {
+
+        /// <summary>
+        /// Return the file name of the logging file for the device.
+        /// </summary>
+        /// <returns>The file name for the logging file.</returns>
+        string GetLoggingFile();
+    }
+}
